4ac453b594673726b062472dc82913f30e0b8327
AntoineHoe <antoinehoe@gmail.com>
2021-05-07 17:09:43 +0200

    calcul du nbr de bridge

--- src/Graph.java
+++ src/Graph.java
@@ -1,5 +1,3 @@
-import com.sun.javafx.collections.MappingChange;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,19 +6,27 @@ import java.util.*;
 
 public class Graph {
 
+    //for bridge count
     int time = 0;
     static final int NIL = -1;
+    int graphSize;
+    int nbrBridge;
 
-    ArrayList<ArrayList<Integer>> adjacencyList;
+    ArrayList<ArrayList<Integer>> adjencyList;
+
 
     ArrayList<String[]> allDesigners;
     ArrayList<String[]> allFollowers;
     ArrayList<String[]> allShots;
 
+    ArrayList<Integer> alreadyDone;
+
     // for component count
     Map<Integer, Boolean> visited;
     int numberOfComponent;
     Map<Integer, HashSet <Integer>> adjencyMap;
+
+    boolean communNeighbor;
 
 
     public ArrayList<String[]>  extract(String name){
@@ -55,22 +61,20 @@ public class Graph {
     }
 
     public void addEdge( int follower, int followed){
-        adjacencyList.get(follower).add(followed);
+        adjencyList.get(follower).add(followed);
         //System.out.println(follower+" a suivi "+followed);
 
     }
 
-
-
     public void showAdjencyList(){
-        for (int i = 0; i < adjacencyList.size(); i++) {
+        for (int i = 0; i < adjencyList.size(); i++) {
 
-            if(!adjacencyList.get(i).isEmpty()){
+            if(!adjencyList.get(i).isEmpty()){
                 System.out.println("\nAdjacency list of vertex " + i);
                 System.out.print("head");
-                for (int j = 0; j < adjacencyList.get(i).size(); j++) {
+                for (int j = 0; j < adjencyList.get(i).size(); j++) {
 
-                    System.out.print(" -> "+adjacencyList.get(i).get(j));
+                    System.out.print(" -> "+ adjencyList.get(i).get(j));
                 }
                 System.out.println();
             }
@@ -83,26 +87,28 @@ public class Graph {
     // visited[] --> keeps tract of visited vertices
     // disc[] --> Stores discovery times of visited vertices
     // parent[] --> Stores parent vertices in DFS tree
-    void bridgeUtil(int u, boolean visited[], int disc[], int low[], int parent[])
+    void bridgeUtil(int u, Map visited, int disc[], int low[], int parent[])
     {
 
         // Mark the current node as visited
-        visited[u] = true;
+        visited.put(u, true);
 
         // Initialize discovery time and low value
         disc[u] = low[u] = ++time;
 
-        // Go through all vertices aadjacent to this
-        Iterator<Integer> i = adjacencyList.get(u).iterator();
+        // Go through all vertices adjacent to this
+        Iterator<Integer> i = adjencyMap.get(u).iterator();
         while (i.hasNext())
         {
             int v = i.next();  // v is current adjacent of u
+            boolean isVisited = (Boolean) visited.get(v);
 
             // If v is not visited yet, then make it a child
             // of u in DFS tree and recur for it.
             // If v is not visited yet, then recur for it
-            if (!visited[v])
-            {
+            if (!isVisited) {
+
+
                 parent[v] = u;
                 bridgeUtil(v, visited, disc, low, parent);
 
@@ -114,7 +120,8 @@ public class Graph {
                 // under v is below u in DFS tree, then u-v is
                 // a bridge
                 if (low[v] > disc[u])
-                    System.out.println(u+"-->"+v);
+                    //System.out.println(u+"-->"+v);
+                    nbrBridge++;
             }
 
             // Update low value of u for parent function calls.
@@ -127,37 +134,41 @@ public class Graph {
     // function bridgeUtil()
     void bridge()
     {
+
         // Mark all the vertices as not visited
-        boolean visited[] = new boolean[adjacencyList.size()];
-        int disc[] = new int[adjacencyList.size()];
-        int low[] = new int[adjacencyList.size()];
-        int parent[] = new int[adjacencyList.size()];
+        //boolean visited[] = new boolean[graphSize];
+        int disc[] = new int[allDesigners.size()];
+        int low[] = new int[allDesigners.size()];
+        int parent[] = new int[allDesigners.size()];
 
 
         // Initialize parent and visited, and ap(articulation point)
         // arrays
-        for (int i = 0; i < adjacencyList.size(); i++)
+        for (int i = 0; i < graphSize; i++)
         {
             parent[i] = NIL;
-            visited[i] = false;
+            visited.put(i, false);
         }
 
         // Call the recursive helper function to find Bridges
         // in DFS tree rooted with vertex 'i'
-        for (int i = 0; i < adjacencyList.size(); i++)
-            if (visited[i] == false)
-                bridgeUtil(i, visited, disc, low, parent);
+        for(Integer node : adjencyMap.keySet()){
+            if(visited.get(node) == false) {
+                bridgeUtil(node, visited, disc, low, parent);
+            }
+        }
+        System.out.println("Number of bridge : "+nbrBridge);
     }
 
     public void nodeWithMoreThanXConnection( int size){
-        for (int i = 0; i < adjacencyList.size(); i++) {
+        for (int i = 0; i < adjencyList.size(); i++) {
 
-            if(adjacencyList.get(i).size()>size){
+            if(adjencyList.get(i).size()>size){
                 System.out.println("\nAdjacency list of vertex " + i);
                 System.out.print("head");
-                for (int j = 0; j < adjacencyList.get(i).size(); j++) {
+                for (int j = 0; j < adjencyList.get(i).size(); j++) {
 
-                    System.out.print(" -> "+adjacencyList.get(i).get(j));
+                    System.out.print(" -> "+ adjencyList.get(i).get(j));
                 }
                 System.out.println();
             }
@@ -203,8 +214,61 @@ public class Graph {
         System.out.println("Number of components : "+numberOfComponent);
     }
     /////////////////////////////////////////////////////////////////
+    /////////////////COMPTER LE NBR DE LOCAL BRIDGES/////////////////
     /////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////
+
+
+    public void recursiveLocalBCounter(){
+        int nbrLocalBridge = 0;
+        //For every node
+        for(Integer node1: adjencyMap.keySet()){
+            HashSet<Integer> allNeighbors1 = adjencyMap.get(node1);
+            if(!allNeighbors1.isEmpty()){
+                //we must check if all of its neighbors
+                for(int neighbor : allNeighbors1){
+                    //share a same neighbor node
+                    HashSet<Integer> neighborOfNeighbor = adjencyMap.get(neighbor);
+                    communNeighbor = false;
+                    for(int element : neighborOfNeighbor){
+                        if(!allNeighbors1.contains(element)){
+                            communNeighbor = true;
+                        }
+                        if(communNeighbor){
+                            nbrLocalBridge++;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("Number of local bridges : "+nbrLocalBridge);
+    }
+
+    public void localBCounter(){
+        int nbrLocalBridge = 0;
+        for(int node1 : adjencyMap.keySet()){
+            for(int node2 : adjencyMap.keySet()){
+                if(node1!=node2 || !alreadyDone.contains(node2)) {
+                    if (common_neighbor(node1, node2)) {
+                        nbrLocalBridge++;
+                    };
+                }
+            }
+            alreadyDone.add(node1);
+        }
+        System.out.println("Nombre de local bridges : "+nbrLocalBridge);
+    }
+
+    public boolean common_neighbor(int u, int v){
+        HashSet<Integer> uNeighbors = adjencyMap.get(u);
+        HashSet<Integer> vNeighbors = adjencyMap.get(v);
+        communNeighbor = false;
+        for(int element : uNeighbors){
+            if(vNeighbors.contains(element)){
+                communNeighbor=true;
+            }
+        }
+        return communNeighbor;
+    }
 /*
     /**
      * Build an adjency list used to manipulate the graph
@@ -231,44 +295,54 @@ public class Graph {
      * @param isDirected : if false then add a link from B->A additionnaly to the A->B
      */
     public void buildQ1(boolean isDirected){
-        for(int i=0; i<allFollowers.size();i++){
 
-            int follower = Integer.valueOf(allFollowers.get(i)[0]);
-            int followed = Integer.valueOf(allFollowers.get(i)[1]);
 
-            adjencyMap.putIfAbsent(followed, new HashSet<Integer>());
-            adjencyMap.putIfAbsent(follower, new HashSet<Integer>());
+        for (String[] allFollower : allFollowers) {
+
+            int follower = Integer.parseInt(allFollower[0]);
+            int followed = Integer.parseInt(allFollower[1]);
+
+            adjencyMap.putIfAbsent(followed, new HashSet<>());
+            adjencyMap.putIfAbsent(follower, new HashSet<>());
 
             visited.put(followed, false);
             visited.put(follower, false);
 
+            this.communNeighbor = false;
+            this.alreadyDone = new ArrayList<>();
+
             adjencyMap.get(follower).add(followed);
-            if(!isDirected){
+
+            if (!isDirected) {
                 adjencyMap.get(followed).add(follower);
+
             }
         }
-
+        this.graphSize = adjencyMap.size();
 
     }
 
     public  Graph(){
 
-        this.adjacencyList = new ArrayList<ArrayList<Integer>>();
-
         this.allShots = extract("shots.csv");
         this.allDesigners = extract("designers.csv");
         this.allFollowers = extract("followers.csv");
+
         this.visited = new HashMap<>();
         this.numberOfComponent = 0;
 
-        this.adjencyMap = new HashMap<Integer, HashSet<Integer>>();
+        this.adjencyList = new ArrayList<>();
+        this.adjencyMap = new HashMap<>();
+        this.nbrBridge = 0;
     }
 
     public static void main(String[] args) {
         Graph graph = new Graph();
         graph.buildQ1(false);
-        //graph.bridge();
         //graph.nodeWithMoreThanXConnection(30);
         graph.componentCounter();
+        graph.recursiveLocalBCounter();
+        graph.localBCounter();
+        graph.bridge();
     }
 }
